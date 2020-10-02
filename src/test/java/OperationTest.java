e3a118bad73d82242220c8fd084d2f28ab57317b
DmitryLebedev <lebedev.dmitry.swan@yandex.ru>
2020-10-02 16:53:34 +0400

    Change
    contentEquals (This method checks to see if the two files are different lengths or if they point to the same file, before resorting to byte-by-byte comparison of the contents.)
    to
    contentEqualsIgnoreEOL (This method checks to see if the two files point to the same file, before resorting to line-by-line comparison of the contents.)
    + set maven default encoding

--- src/test/java/OperationTest.java
+++ src/test/java/OperationTest.java
@@ -1,11 +1,10 @@
+import static org.apache.commons.io.FileUtils.contentEqualsIgnoreEOL;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
-
-import static org.apache.commons.io.FileUtils.contentEquals;
 
 public class OperationTest {
     private static final String TARGET_TEST_RESOURCES_PATH = "target/test-classes/";
@@ -29,9 +28,10 @@ public class OperationTest {
                         "-o",  targetOutputFilePath,
                         "-op", operationType
                 });
-         Assert.assertTrue(contentEquals(
+         Assert.assertTrue(contentEqualsIgnoreEOL(
                  new File(targetOutputFilePath),
-                 new File(TARGET_TEST_RESOURCES_PATH + outputFileExpectedPath)
+                 new File(TARGET_TEST_RESOURCES_PATH + outputFileExpectedPath),
+                 "UTF-8"
          ));
     }
 }
